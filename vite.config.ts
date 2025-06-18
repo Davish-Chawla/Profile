f1b2f918dd2cc59fbf3b49bdc40a1de6bb214d9a
Davish-chawla <davishchawla99@gmail.com>
2025-06-18 15:34:50 +0530

    Add images and PDFs

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // <-- add this
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
     minify: 'terser',
-    target: 'esnext', // better suited for modern browsers
+    target: 'esnext',
     rollupOptions: {
       output: {
         manualChunks: undefined,
@@ -18,6 +18,6 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true, // optional: auto open in browser
+    open: true,
   },
 });
